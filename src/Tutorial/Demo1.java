c9c834ce10d6c3afa63620809e55d564dd4abe7b
Aarush <Aarush@DESKTOP-FJ7VKA0>
2022-12-17 15:59:33 +0530

    Sonalis code

--- src/Tutorial/Demo1.java
+++ src/Tutorial/Demo1.java
@@ -3,8 +3,7 @@ package Tutorial;
 public class Demo1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
---
+++ src/Tutorial/Demo2.java
@@ -0,0 +1,12 @@
+package Tutorial;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		a=10;b=20;
+		c=a+b;
+System.out.println(c);
+	}
+
+}
